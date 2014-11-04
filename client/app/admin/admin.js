5399259e5c900a0028d55a2b85f59c0b6364e704
Sreevisakh <mail@sreevisakh.com>
2014-11-04 17:32:36 +0530

    First commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('preciousApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
